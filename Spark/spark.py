f03bb7740b140a4b88b480c179055a338f48b2c9
Xi Chen <jasonxi89@gmail.com>
2019-03-27 17:12:45 -0700

    updated

--- Spark/spark.py
+++ Spark/spark.py
@@ -1,19 +1,24 @@
-# from pyspark import SparkConf, SparkContext
+# # from pyspark import SparkConf, SparkContext
+# #
+# # #创建sparkconf:设置spark的相关信息
+# # con = SparkConf().setMaster("local[2]").setAppName("spark0325")
+# #
+# # #创建sparkcontext
+# # sc = SparkContext(conf = con)
+# #
+# # #业务逻辑
+# # data = [i for i in range(1,6)]
+# # disData = sc.parallelize(data)
+# # print(disData.collect())
+# #
+# #
+# # sc.stop()
+# #
+# #
+# from pyspark.sql import SparkSession
 #
-# #创建sparkconf:设置spark的相关信息
-# con = SparkConf().setMaster("local[2]").setAppName("spark0325")
+# data = [('a',5),('b',7),('c',6),('d',1)]
+# print(sorted(data,key=lambda x:x[1],reverse=True))
 #
-# #创建sparkcontext
-# sc = SparkContext(conf = con)
-#
-# #业务逻辑
-# data = [i for i in range(1,6)]
-# disData = sc.parallelize(data)
-# print(disData.collect())
-#
-#
-# sc.stop()
-#
-#
-data = [('a',5),('b',7),('c',6),('d',1)]
-print(sorted(data,key=lambda x:x[1],reverse=True))
+# spark = SparkSession.builder.appName("spark0801").getOrCreate()
+# spark.stop()
